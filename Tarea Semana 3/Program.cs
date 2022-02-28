38038c0694957c1186c80a151f1796c72807867c
pc <pc@DESKTOP-TQGQV41>
2022-02-27 22:28:03 -0600

    Commit Tarea 3

---
+++ Tarea Semana 3/Program.cs
@@ -0,0 +1,374 @@
+﻿static void desglosecantidad()
+{
+    //int CAN, C100, C50, C20, C10, C5;
+    //CAN = C100 = C50 = C20 = C10 = C5 = 0;
+    //string linea;
+
+    //decimal cantidad_moneda;// = 0.0d;
+
+
+
+    //Console.Write("Ingrese una cantidad: ");
+    //linea = Console.ReadLine();
+    ////CAN = Convert.ToInt32(linea);
+
+
+    //cantidad_moneda = Convert.ToDecimal(linea);
+    ////cantidad_moneda = (cantidad_moneda * 7.89m); //convierte de dolares a Quetzales
+
+
+
+    //CAN = Convert.ToInt32(Math.Truncate(cantidad_moneda));
+
+    //cantidad_moneda = (cantidad_moneda - CAN) * 100;
+    //Console.WriteLine($"Centavos = {cantidad_moneda / 100}");
+
+
+    ////cantidades billetes:
+    //if ((CAN >= 100))
+    //{
+    //    C100 = (CAN / 100);
+    //    CAN = CAN - (C100 * 100);
+    //}
+
+    //if ((CAN >= 50))
+    //{
+    //    C50 = (CAN / 50);
+    //    CAN = CAN - (C50 * 50);
+    //}
+
+    //if ((CAN >= 20))
+    //{
+    //    C20 = (CAN / 20);
+    //    CAN = CAN - (C20 * 20);
+    //}
+
+    //if ((CAN >= 10))
+    //{
+    //    C10 = (CAN / 10);
+    //    CAN = CAN - (C10 * 10);
+    //}
+
+    //if (CAN >= 5)
+    //{
+    //    C5 = (CAN / 5);
+    //    CAN = CAN - (C5 * 5);
+    //}
+
+    //Console.WriteLine($"--- ESTOS SON TUS BILLETES ---");
+    //Console.WriteLine($"BILLETES DE 100: {C100}");
+    //Console.WriteLine($"BILLETES DE 50: {C50}");
+    //Console.WriteLine($"BILLETES DE 20: {C20}");
+    //Console.WriteLine($"BILLETES DE 10: {C10}");
+    //Console.WriteLine($"BILLETES DE 5: {C5}");
+    //Console.WriteLine($"BILLETES DE 1: {CAN}");
+
+
+    //////monedas
+    //int MONE, MONE50, MONE25, MONE10, MONE5, MONE1;
+    //MONE = MONE50 = MONE25 = MONE10 = MONE5 = MONE1 = 0;
+    //MONE = Convert.ToInt32(cantidad_moneda);
+    //if ((MONE >= 50))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE50 = (MONE / 50);
+    //    MONE = MONE - (MONE50 * 50);
+    //}
+
+    //if ((MONE >= 25))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE25 = (MONE / 25);
+    //    MONE = MONE - (MONE25 * 25);
+    //}
+
+    //if ((MONE >= 10))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE10 = (MONE / 10);
+    //    MONE = MONE - (MONE10 * 10);
+    //}
+
+    //if ((MONE >= 5))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE5 = (MONE / 5);
+    //    MONE = MONE - (MONE5 * 5);
+    //}
+
+    //Console.WriteLine($"--- ESTOS SON TUS monedas ---");
+    //Console.WriteLine($"MONEDA DE 50: {MONE50}");
+    //Console.WriteLine($"MONEDA DE 25: {MONE25}");
+    //Console.WriteLine($"MONEDA DE 10: {MONE10}");
+    //Console.WriteLine($"MONEDA DE 5: {MONE5}");
+    //Console.WriteLine($"MONEDA DE 1: {MONE}");
+
+}
+desglosecantidad();
+
+
+static void desglose()
+{
+    //int CAN, C200,C100, C50, C20, C10, C5;
+    //CAN = C200 = C100 = C50 = C20 = C10 = C5 = 0;
+    //string linea;
+
+    //decimal cantidad_moneda;// = 0.0d;
+    //decimal TIPO_CAMBIO = 7.65m;
+
+    //Console.Write("Ingrese una cantidad en dolares:");
+    //linea = Console.ReadLine();
+    ////CAN = Convert.ToInt32(linea);
+
+
+    //cantidad_moneda = Convert.ToDecimal(linea);
+    ////cantidad_moneda = (cantidad_moneda * 7.89m); //convierte de dolares a Quetzales
+
+    //cantidad_moneda = (cantidad_moneda * TIPO_CAMBIO); //convierte de dolares a Quetzales
+
+    //Console.WriteLine($"convirtiendo a Quetzales {cantidad_moneda}");
+
+    //CAN = Convert.ToInt32(Math.Truncate(cantidad_moneda));
+
+    //cantidad_moneda = (cantidad_moneda - CAN) * 100;
+    //Console.WriteLine($"Monedas = {cantidad_moneda}");
+
+
+    ////cantidades billetes:
+    //if ((CAN >= 200))
+    //{
+    //    C200 = (CAN / 200);
+    //    CAN = CAN - (C200 * 200);
+    //}
+    //if ((CAN >= 100))
+    //{
+    //    C100 = (CAN / 100);
+    //    CAN = CAN - (C100 * 100);
+    //}
+
+    //if ((CAN >= 50))
+    //{
+    //    C50 = (CAN / 50);
+    //    CAN = CAN - (C50 * 50);
+    //}
+
+    //if ((CAN >= 20))
+    //{
+    //    C20 = (CAN / 20);
+    //    CAN = CAN - (C20 * 20);
+    //}
+
+    //if ((CAN >= 10))
+    //{
+    //    C10 = (CAN / 10);
+    //    CAN = CAN - (C10 * 10);
+    //}
+
+    //if (CAN >= 5)
+    //{
+    //    C5 = (CAN / 5);
+    //    CAN = CAN - (C5 * 5);
+    //}
+
+    //Console.WriteLine($"--- ESTOS SON TUS BILLETES ---");
+    //Console.WriteLine($"BILLETES DE 200: {C200}");
+    //Console.WriteLine($"BILLETES DE 100: {C100}");
+    //Console.WriteLine($"BILLETES DE 50: {C50}");
+    //Console.WriteLine($"BILLETES DE 20: {C20}");
+    //Console.WriteLine($"BILLETES DE 10: {C10}");
+    //Console.WriteLine($"BILLETES DE 5: {C5}");
+    //Console.WriteLine($"BILLETES DE 1: {CAN}");
+
+
+    //////monedas
+    //int MONE, MONE50, MONE25, MONE10, MONE5, MONE1;
+    //MONE = MONE50 = MONE25 = MONE10 = MONE5 = MONE1 = 0;
+    //MONE = Convert.ToInt32(cantidad_moneda);
+    //if ((MONE >= 50))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE50 = (MONE / 50);
+    //    MONE = MONE - (MONE50 * 50);
+    //}
+
+    //if ((MONE >= 25))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE25 = (MONE / 25);
+    //    MONE = MONE - (MONE25 * 25);
+    //}
+
+    //if ((MONE >= 10))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE10 = (MONE / 10);
+    //    MONE = MONE - (MONE10 * 10);
+    //}
+
+    //if ((MONE >= 5))
+    //{
+    //    //MONE50 = ((int)cantidad_moneda / 100);
+    //    MONE5 = (MONE / 5);
+    //    MONE = MONE - (MONE5 * 5);
+    //}
+
+    //Console.WriteLine($"--- ESTOS SON TUS monedas ---");
+    //Console.WriteLine($"MONEDA DE 50: {MONE50}");
+    //Console.WriteLine($"MONEDA DE 25: {MONE25}");
+    //Console.WriteLine($"MONEDA DE 10: {MONE10}");
+    //Console.WriteLine($"MONEDA DE 5: {MONE5}");
+    //Console.WriteLine($"MONEDA DE 1: {MONE}");
+
+}
+desglose();
+
+
+static void desgloseswitch()
+{
+    int CAN, C200, C100, C50, C20, C10, C5;
+    CAN = C200 = C100 = C50 = C20 = C10 = C5 = 0;
+    string linea;
+
+    decimal cantidad_moneda;// = 0.0d;
+
+
+
+    Console.Write("Ingrese una cantidad: ");
+    linea = Console.ReadLine();
+    //CAN = Convert.ToInt32(linea);
+
+
+    cantidad_moneda = Convert.ToDecimal(linea);
+    //cantidad_moneda = (cantidad_moneda * 7.89m); //convierte de dolares a Quetzales
+
+
+
+    CAN = Convert.ToInt32(Math.Truncate(cantidad_moneda));
+
+    cantidad_moneda = (cantidad_moneda - CAN) * 100;
+    Console.WriteLine($"Centavos = {cantidad_moneda / 100}");
+
+    while (CAN >= 5)
+    {
+
+        switch (CAN)
+        {
+            case >= 200:
+                {
+                    C200 = (CAN / 200);
+                    CAN = CAN - (C200 * 200);
+                    break;
+                }
+            case >= 100:
+                {
+                    C100 = (CAN / 100);
+                    CAN = CAN - (C100 * 100);
+                    break;
+                }
+            case >= 50:
+                {
+                    C50 = (CAN / 50);
+                    CAN = CAN - (C50 * 50);
+                    break;
+                }
+            case >= 20:
+                {
+                    C20 = (CAN / 20);
+                    CAN = CAN - (C20 * 20);
+                    break;
+                }
+
+            case >= 10:
+                {
+                    C10 = (CAN / 10);
+                    CAN = CAN - (C10 * 10);
+                    break;
+                }
+
+            case >= 5:
+                {
+                    C5 = (CAN / 5);
+                    CAN = CAN - (C5 * 5);
+                    break;
+                }
+        }
+    }
+    //cantidades billetes:
+
+    Console.WriteLine($"--- ESTOS SON TUS BILLETES ---");
+    Console.WriteLine($"BILLETES DE 200: {C200}");
+    Console.WriteLine($"BILLETES DE 100: {C100}");
+    Console.WriteLine($"BILLETES DE 50: {C50}");
+    Console.WriteLine($"BILLETES DE 20: {C20}");
+    Console.WriteLine($"BILLETES DE 10: {C10}");
+    Console.WriteLine($"BILLETES DE 5: {C5}");
+    Console.WriteLine($"BILLETES DE 1: {CAN}");
+
+
+    ////monedas
+    int M, M50, M25, M10, M5, M1;
+    M = M50 = M25 = M10 = M5 = M1 = 0;
+    M = Convert.ToInt32(cantidad_moneda);
+
+    while (M >= 5)
+    {
+
+        switch (M)
+        {
+            case >= 50:
+                {
+                    M50 = ((int)cantidad_moneda / 100);
+                    M50 = (M / 50);
+                    M = M - (M50 * 50);
+                    break;
+                }
+            case >= 25:
+                {
+                    M25 = ((int)cantidad_moneda / 100);
+                    M25 = (M / 25);
+                    M = M - (M25 * 25);
+                    break;
+                }
+            case >= 10:
+                {
+                    M10 = ((int)cantidad_moneda / 100);
+                    M10 = (M / 10);
+                    M = M - (M10 * 10);
+                    break;
+                }
+            case >= 5:
+                {
+                    M5 = ((int)cantidad_moneda / 100);
+                    M5 = (M / 5);
+                    M = M - (M5 * 5);
+                    break;
+                }
+            case >= 1:
+                {
+                    M1 = ((int)cantidad_moneda / 100);
+                    M1 = (M / 1);
+                    M = M - (M1 * 1);
+                    break;
+                }
+
+
+                Console.WriteLine($"--- ESTOS SON TUS monedas ---");
+                Console.WriteLine($"MONEDA DE 50: {M50}");
+                Console.WriteLine($"MONEDA DE 25: {M25}");
+                Console.WriteLine($"MONEDA DE 10: {M10}");
+                Console.WriteLine($"MONEDA DE 5: {M5}");
+                Console.WriteLine($"MONEDA DE 1: {M1}");
+
+
+        }
+    }
+}
+
+desgloseswitch();
+
+
+
+
+
+
+
